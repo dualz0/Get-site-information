7154342c5e318ea99dbc6850960f9765d10d2d62
dualz0 <50540561+dualz0@users.noreply.github.com>
2020-03-20 22:54:10 +0800

    Basic function is realized.

---
+++ site_info/background.js
@@ -0,0 +1,5 @@
+﻿chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+      code: 'alert("[" + document.title + "](" + window.location.href + ")")'
+    });
+  });
